81e902d1f9d3c424a6d412be84c1d69985ff8218
yuukixyz <0xv75b42326631e@au.com>
2024-05-31 00:29:53 +0000

    "/" にファイルをアップロード

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='tjaf',
+    version='1.0.7',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    author='Yuuki',
+    author_email='0xv75b42326631e@au.com',
+    description='TJA decoder in Python',
+    long_description='Easy to use TJA decoder in Python for everyone.',
+    url='',
+)
